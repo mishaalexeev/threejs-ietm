5c06cca45dc194f3441c875b7c6f4163107cf399
Vsevolod <sevkaplay@gmail.com>
2021-04-21 21:38:11 +0300

    Add handling item selecting in tree view

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,40 +1,20 @@
 import { Menu, Tree } from "antd";
 import React, { FC } from "react";
 import "./menu.css";
-import {
-  ApartmentOutlined,
-  DownOutlined,
-  MailOutlined,
-  ThunderboltOutlined,
-  ToolOutlined,
-  // @ts-ignore
-} from "@ant-design/icons";
+import { DownOutlined } from "@ant-design/icons";
 import withStores from "hocs/withStores";
 import { RootStore } from "store";
 import { treeData } from "data/treeData";
+import { Key } from "antd/es/table/interface";
 
 type Props = {
   stores: RootStore;
 };
 
-const { SubMenu } = Menu;
-
-// submenu keys of first level
-const rootSubmenuKeys = ["sub1"];
-
 const MenuMain: FC<Props> = ({ stores }) => {
-  const [openKeys, setOpenKeys] = React.useState(["sub1"]);
-
-  const onOpenChange = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
-  const handleItemClicked = ({ key }) => {
+  const handleItemSelect = (selectedKeys: Key[]) => {
+    if (selectedKeys.length > 1) throw new Error("Выбрано больше 1 элемента");
+    const [key] = selectedKeys;
     switch (key) {
       case "Разборка редуктора":
         stores.modelStore.startAnimation();
@@ -50,6 +30,7 @@ const MenuMain: FC<Props> = ({ stores }) => {
       showLine
       showIcon
       switcherIcon={<DownOutlined />}
+      onSelect={handleItemSelect}
       treeData={treeData}
       className="tree-view"
     />
